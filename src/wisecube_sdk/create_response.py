81f9b38756df0d45ae2a46dac6520b15963246c6
alex <Therim13!>
2024-02-09 10:10:55 -0800

    added pandas support

--- src/wisecube_sdk/create_response.py
+++ src/wisecube_sdk/create_response.py
@@ -1,18 +1,29 @@
 def qa(response):
-    return response.json()
+    response = response.json()
+    return pd.DataFrame(response["data"]["summaryInsights"][0]["data"]["answers"][0]["document"])
 
 
 def documents(response):
-    return response.json()
+    response = response.json()
+    return pd.DataFrame(response["data"]["summaryInsights"][0]["data"]["answers"][0]["document"])
 
 
 def search_graph(response):
-    return response.json()
+    response = response.json()
+    nodes = pd.DataFrame(response["data"]["graphInsights"]["data"]["nodes"])
+    nodes = pd.concat((pd.DataFrame([response["data"]["graphInsights"]["data"]["rootNode"]]), nodes))
+    edges = pd.DataFrame(response["data"]["graphInsights"]["data"]["edges"])
+    return nodes, edges
 
 
 def search_text(response):
-    return response.json()
+    response = response.json()
+    return pd.DataFrame(response["data"]["searchAsYouType"]["data"]["searchLabels"])
 
 
 def advanced_search(response):
+    response = response.json()
+    columns = response["data"]["advancedSearchGraph"]["head"]["vars"]
+    rows = [{k: d["value"] for k, d in r.items()} for r in response["data"]["advancedSearchGraph"]["results"]["bindings"]]
+    pd.DataFrame(rows)[columns]
     return response.json()
