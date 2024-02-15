83a4b69cdeee78e7c7d3b0d28aed42079c6ec723
pratacosmin <mcosmin1995@gmail.com>
2024-02-15 10:54:51 +0200

    fix models on sagemaker

--- src/wisecube_sdk/model_names.py
+++ src/wisecube_sdk/model_names.py
@@ -2,34 +2,34 @@ from enum import Enum
 
 
 class WisecubeModel(Enum):
-    BBB = "BBB"  # ok
-    LOGS = "logS" # ok
-    CYP2CI9I = "CYP2CI9i" # ok
-    LOGD7 = "LogD7" # ok
-    PGPI = "PGPi" # ok
-    PGPS = "PGPs" #ok
-    HIA = "HIA" #ok
-    F20 = "F20" #ok
-    F30 = "F30" #ok
-    PPB = "PPB" #ok
-    VD = "VD" #ok
-    CYPIA2I = "CYPIA2i" #ok
-    CYPIA2S = "CYPIA2s" #ok
-    CYP3A4I = "CYP3A4i" #ok
-    CYP3A4S = "CYP3A4s" #ok
-    CYP2C9I = "CYP2C9i" #ok
-    CYP2C9S = "CYP2C9s" #ok
-    CYP2C19S = "CYP2C19s" #ok
-    CYP2D6I = "CYP2D6i" #ok
-    CYP2D6S = "CYP2D6s" #ok
-    CL = "CL" #ok
-    AMES = "Ames" #ok
-    DILI = "DILI" # #ok
-    SKINS = "SkinS" #ok
-    CACO2 = "Caco2" #  error
-    # THALF = "THALF" # error
-    # HERG = "hERG" # error
-    # HHT = "HHT" # error
+    BBB = "BBB"  
+    LOGS = "logS" 
+    CYP2CI9I = "CYP2CI9i" 
+    LOGD7 = "LogD7" 
+    PGPI = "PGPi" 
+    PGPS = "PGPs" 
+    HIA = "HIA" 
+    F20 = "F20" 
+    F30 = "F30" 
+    PPB = "PPB" 
+    VD = "VD" 
+    CYPIA2I = "CYPIA2i" 
+    CYPIA2S = "CYPIA2s" 
+    CYP3A4I = "CYP3A4i" 
+    CYP3A4S = "CYP3A4s" 
+    CYP2C9I = "CYP2C9i" 
+    CYP2C9S = "CYP2C9s" 
+    CYP2C19S = "CYP2C19s" 
+    CYP2D6I = "CYP2D6i" 
+    CYP2D6S = "CYP2D6s" 
+    CL = "CL" 
+    AMES = "Ames" 
+    DILI = "DILI"  
+    SKINS = "SkinS" 
+    CACO2 = "Caco2"   
+    THALF = "THALF"  
+    HERG = "hERG"  
+    HHT = "HHT"  
 
 
 
