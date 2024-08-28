9e928def13469b4402845e71e7b4a21cde5e982c
larisa <larisa@wisecube.ai>
2024-08-28 12:26:52 +0300

    exception catch for vector_function

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="wisecube",
-    version="1.0.7",
+    version="1.0.8",
     author="Wisecube",
     author_email="info@wisecube.ai",
     description="Wisecube SDK for graph Search",
